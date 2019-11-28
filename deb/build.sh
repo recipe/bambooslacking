8f5d7b13f7e1fab6c4c3fbfa16d514ac1e7732bc
Vitaliy Demidov <vital.letter@gmail.com>
2019-11-28 23:10:38 +0200

    add .gitignore

--- deb/build.sh
+++ deb/build.sh
@@ -3,17 +3,21 @@
 set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+VERSION="1.0-1"
 
 # it depends on your build settings
-cp $DIR/../build/release/bambooslacking $DIR/bambooslacking_1.0-1/usr/local/bin/bambooslacking
+cp $DIR/../build/release/bambooslacking $DIR/bambooslacking_$VERSION/usr/local/bin/bambooslacking
 
-chmod a+x $DIR/bambooslacking_1.0-1/etc/init.d/bambooslacking \
-$DIR/bambooslacking_1.0-1/usr/local/bin/bambooslacking \
-$DIR/bambooslacking_1.0-1/DEBIAN/postinst \
-$DIR/bambooslacking_1.0-1/DEBIAN/postrm
+chmod a+x $DIR/bambooslacking_$VERSION/etc/init.d/bambooslacking \
+$DIR/bambooslacking_$VERSION/usr/local/bin/bambooslacking \
+$DIR/bambooslacking_$VERSION/DEBIAN/postinst \
+$DIR/bambooslacking_$VERSION/DEBIAN/postrm
 
-dpkg-deb --build $DIR/bambooslacking_1.0-1
+dpkg-deb --build $DIR/bambooslacking_$VERSION
 
-# port binary files back to repo
-cp $DIR/../build/release/bambooslacking /deb/bambooslacking_1.0-1/usr/local/bin/
-cp $DIR/bambooslacking_1.0-1.deb /deb/
+# port binary files back to repo in case vagrant deployment is being used
+VAGRANT_DIR="/deb/bambooslacking_$VERSION"
+if [ -d "$VAGRANT_DIR" ]; then
+cp $DIR/../build/release/bambooslacking $VAGRANT_DIR/usr/local/bin/
+cp $DIR/bambooslacking_$VERSION.deb /deb/
+fi
